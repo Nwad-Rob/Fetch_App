a82222e404cbace67e45b204f95d5ee7c500cc56
Nwad-Rob <rnnwadinobi@gmail.com>
2023-07-02 17:51:45 -0700

    Created the item class, parsed the JSON file and added buttons

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Classes/Item.java
@@ -0,0 +1,35 @@
+package com.example.myapplication.Classes;
+
+public class Item {
+    private int id;
+    private int listId;
+    private String name;
+
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getListId() {
+        return this.listId;
+    }
+
+
+
+    public void setListId(int listId) {
+        this.listId = listId;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,15 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+import com.example.myapplication.Classes.Item;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +19,8 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button buttonE;
     TextView txt;
+
+    Item item = new Item();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +32,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-        
+        // Obtain the JSON url
+        String url = "https://fetch-hiring.s3.amazonaws.com/hiring.json";
+        try {
+            String jsonData = getHTML(url);
+            List<Item> items = parseJSON(jsonData);
+            displayItems(items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println(getHTML(url));
+
+        System.out.println(parseJSON(url));
+    }
+
+    //Creating connection, obtain the Json Object and transform it into a string
+    public static String getHTML(String urlToRead) throws IOException {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToRead);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(conn.getInputStream()))) {
+            for (String line; (line = reader.readLine()) != null; ) {
+                result.append(line);
+            }
+        }
+        return result.toString();
+    }
+
+    //parsing through a Json Object
+    private static List<Item> parseJSON(String jsonData) {
+        Gson gson = new Gson();
+        return gson.fromJson(jsonData, new TypeToken<List<Item>>() {}.getType());
+    }
+
+    private static void displayItems(List<Item> items) {
+        Map<Integer, List<Item>> itemsByListId = items.stream()
+                .filter(item -> item.getName() != null && !item.getName().isEmpty())
+                .collect(Collectors.groupingBy(Item::getListId));
+
+        itemsByListId.forEach((listId, itemList) -> {
+            itemList.sort(Comparator.comparing(Item::getName));
+            System.out.println("List ID: " + listId);
+            itemList.forEach(item -> System.out.println("  Item Name: " + item.getName()));
+            System.out.println();
+        });
     }
 }
